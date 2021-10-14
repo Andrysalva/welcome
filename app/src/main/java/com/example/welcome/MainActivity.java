b33aab3506f41482a16540d5f1124d337af1fb8b
Andrysalva <a.salvatori38@gmail.com>
2021-10-14 18:11:09 +0200

    fase 2: aggiunta funzione al tasto "test"

--- app/src/main/java/com/example/welcome/MainActivity.java
+++ app/src/main/java/com/example/welcome/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.welcome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button reset;
+    Button test;
+    TextView output;
+    EditText nome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        reset = findViewById(R.id.reset_button);
+        nome = findViewById(R.id.name);
+        test = findViewById(R.id.test_button);
+        output = findViewById(R.id.output);
     }
+
+    public void cancella(View view){
+        nome.setText("");
+    }
+
+    public void mostra_testo(View view){
+        output.setText("");
+        output.setText(nome.getText());
+    }
+
 }
