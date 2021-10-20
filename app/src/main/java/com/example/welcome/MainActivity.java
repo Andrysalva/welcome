b24cad88489adedfb74120b58fbb9cc248bff777
Andrysalva <a.salvatori38@gmail.com>
2021-10-20 13:02:47 +0200

    EDIT: fase 3, aggiunta nuova activity e la funzione play che cambia le due activity

--- app/src/main/java/com/example/welcome/MainActivity.java
+++ app/src/main/java/com/example/welcome/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.welcome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -38,6 +39,10 @@ public class MainActivity extends AppCompatActivity {
         Toast toast = Toast.makeText(context,nome.getText(), duration);
         toast.show();
         nome.setText("");
+    }
+    public void cambia_schermata(View view){
+        Intent intent = new Intent(context,MainActivity2.class);
+        startActivity(intent);
     }
 
 }
--- app/src/main/java/com/example/welcome/MainActivity2.java
+++ app/src/main/java/com/example/welcome/MainActivity2.java
@@ -3,12 +3,14 @@ package com.example.welcome;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    TextView testo2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        testo2 = (TextView) findViewById(R.id.testo2);
     }
 }
